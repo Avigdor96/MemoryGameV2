180abf13872792ab55c6a97583c533d5e6a5f25c
Avigdor96 <a0556691387@gmail.com>
2025-02-16 01:19:12 +0200

    commit16_02_25

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,19 +1,4 @@
 // document.addEventListener('DOMContentLoaded', ()=>{
-//     const scoreboard = document.getElementById('scoreboard').getElementsByTagName('tbody')[0]
-//     const users = JSON.parse(localStorage.getItem('users')) || {}
-//     for(const player in users){
-//         const row = scoreboard.insertRow()
-//         const cellPlayer = row.insertCell(0)
-//         const cellScore = row.insertCell(1)
-//         const cellCorrect = row.insertCell(2)
-//         const cellUnCorrect= row.insertCell(3)
-
-//         cellPlayer.textContent = player
-//         cellScore.textContent = users[player].score
-//         cellCorrect.textContent = users[player].correct
-//         cellUnCorrect.textContent = users[player].uncorrect
-//     }
-// });
 window.onload =()=>{
     const scoreboard = document.getElementById('scoreboard').getElementsByTagName('tbody')[0]
     const users = JSON.parse(localStorage.getItem('users')) || {}
@@ -23,10 +8,9 @@ window.onload =()=>{
         const cellScore = row.insertCell(1)
         const cellCorrect = row.insertCell(2)
         const cellUnCorrect= row.insertCell(3)
-
         cellPlayer.textContent = player
         cellScore.textContent = users[player].score
         cellCorrect.textContent = users[player].correct
-        cellUnCorrect.textContent = users[player].unCorrect
+        cellUnCorrect.textContent = users[player].uncorrect
     }
 };
